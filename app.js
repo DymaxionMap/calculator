6367d40af0ce78307851f7decc00ebf55f34c819
DymaxionMap <DymaxionMap@users.noreply.github.com>
2015-02-28 18:57:48 -0600

    Save digit keypresses to memory

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ var Calculator = function() {
   this.y = null;
   this.buffer = [];
   this.operation = null;
+  console.log("Created calculator.");
 }
 
 Calculator.prototype.addToBuffer = function (digit) {
+  console.log("In addToBuffer()");
   this.buffer.push(digit);
 };
 
+// Add these in next commit:
+/*
+Calculator.prototype.parseBuffer = function() {
+  if (this.buffer.length === 1 && this.buffer[0] === ".") {
+    this.x = 0;
+  } else {
+    this.x =  Number(buffer.join(""));
+  }
+};
+
+Calculator.prototype.display = function(display) {
+  display.text(x);
+};
+*/
+
 $(document).ready(function() {
   var calc = new Calculator();
+  console.log(calc.buffer);
+  console.log(Object.getOwnPropertyNames(calc));
 
+  $(".keypad").on("click", ".digit", function() {
+    var digit = $(this).data("value");
+    calc.addToBuffer(digit);
+    console.log(calc.buffer);
+    //calc.parseBuffer();
+    //calc.display();
+  });
 });
