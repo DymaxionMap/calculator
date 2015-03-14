837823362e15e8da58d693d59b895af4940f44f3
DymaxionMap <DymaxionMap@users.noreply.github.com>
2015-03-14 15:13:29 -0500

    Add dependencies

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
-
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
